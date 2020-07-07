16e3163d2fbb3e7a6129e83a0a68b93a540c76e6
Bohdan Tverdoukh <b.tverdoukh@relevant.software>
2020-07-07 13:27:00 +0300

    Return class oject

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import SmartBanner from './smartbanner.js';
 
-let smartbanner;
-
-window.addEventListener('load', function() {
-  smartbanner = new SmartBanner();
-  if (smartbanner.apiEnabled) {
-    window.smartbanner = smartbanner;
-  } else {
-    smartbanner.publish();
-  }
-});
+export default SmartBanner;
